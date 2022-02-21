f084b26f768e6a5f2ef1d5eb4e76ab58b186228d
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-02-21 12:07:11 +0900

    slick導入

--- js/changeslide.js
+++ js/changeslide.js
@@ -1,51 +1,64 @@
 $(function(){
 
 
-    let width = $('.carousel-list').outerWidth(true);
-    let length = $('.carousel-list').length;
-    let slideArea = width * length;
-    $('.carousel-area').css('width',slideArea);
+    $('.slider').slick({
+        autoplay: true,         //自動再生
+        autoplaySpeed: 2000,//自動再生のスピード
+        speed: 800,      //スライドするスピード
+        dots: true,//スライドしたのドット
+        arrows: true,          //左右の矢印
+        infinite: true,//スライドのループ
+        pauseOnHover: false,   //ホバーしたときにスライドを一時停止しない　
+        centerMode: true,
+        centerPadding: '25%',
+    });
 
-    let slideCurrent = 0;
-    let lastCurrent = $('.carousel-list').length -1;
+
+    // let width = $('.carousel-list').outerWidth(true);
+    // let length = $('.carousel-list').length;
+    // let slideArea = width * length;
+    // $('.carousel-area').css('width',slideArea);
+
+    // let slideCurrent = 0;
+    // let lastCurrent = $('.carousel-list').length -1;
 
     //スライド切り替わり　左にずれる
 
-    function changeslide(){
-        $('.carousel-area').stop().animate({
-            left: slideCurrent * -width
-        })
+    // function changeslide(){
+    //     $('.carousel-area').stop().animate({
+    //         left: slideCurrent * -width
+    //     })
 
         //pagination
 
-        let pagination = slideCurrent + 1;
-        $('.pagination-circle').removeClass('target');
-        $(".pagination-circle:nth-of-type("+ pagination +")").addClass('target');
-    };
+    //     let pagination = slideCurrent + 1;
+    //     $('.pagination-circle').removeClass('target');
+    //     $(".pagination-circle:nth-of-type("+ pagination +")").addClass('target');
+    // };
 
     //ボタンクリック時にchangeslideを呼び出す
 
-    $('.js-btn-next').click(function(){
+    // $('.js-btn-next').click(function(){
         
-        if(slideCurrent === lastCurrent){
-            console.log('aaa');
-            slideCurrent = 0;
-            changeslide();
-        }else{
-            console.log('bbbb');
-            slideCurrent++;
-            changeslide();
-        };
-    });
+    //     if(slideCurrent === lastCurrent){
+    //         console.log('aaa');
+    //         slideCurrent = 0;
+    //         changeslide();
+    //     }else{
+    //         console.log('bbbb');
+    //         slideCurrent++;
+    //         changeslide();
+    //     };
+    // });
 
-    $('.js-btn-back').click(function(){
+    // $('.js-btn-back').click(function(){
         
-        if(slideCurrent === 0){
-            slideCurrent  = lastCurrent;
-            changeslide();
-        }else{
-            slideCurrent--;
-            changeslide();
-        }
-    });
+    //     if(slideCurrent === 0){
+    //         slideCurrent  = lastCurrent;
+    //         changeslide();
+    //     }else{
+    //         slideCurrent--;
+    //         changeslide();
+    //     }
+    // });
 });
